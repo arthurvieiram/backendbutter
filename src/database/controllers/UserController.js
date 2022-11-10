4f98fff323f7534a03ddafbf84ebeac1f85d6435
Jean Lopes <pierry.jeann@gmail.com>
2022-11-10 20:00:50 -0300

    Continuação do firebase

--- src/database/controllers/UserController.js
+++ src/database/controllers/UserController.js
@@ -1,9 +1,25 @@
 const User = require("../../models/User");
+const firebase = require("../utilities/firebase");/*Chamando a função do firebase*/
 
 module.exports = {
     async create(request, response) {
         try {
             const newUser = request.body;
+            
+            /*adicionando firebase*/
+            const uid = await firebase.createNewUser(user.email, user.password);
+            delete user.password;
+            user.firebase_id = uid;
+            /* 1) adicionar no nosso diagrama uml o campo "firebase_id"
+             2) rodar npx knex migrate:rollback (desfaz todas as migrations feitas)
+             3) na Migration user colocar "table.string("firebase_id").notNullable();"
+             4) Depois disso, salvar e rodar todas e dar um "npx knex migrate:lastest (vai rodar todas as migrations")
+             5) última coisa : ir em uservallidator "que ainda não está criado"
+              na parte de "create" colocar "password: Joi.string().min(aqui vai ser o tam. senha).required(),"
+              fazer o mesmo para name: e email:
+              Após isso temos que dar npm start e testar no inmsonia pra ver se tá tudo certo.
+              */
+             
             const result = await User.create(newUser);
 
             return response.status(200).json(result);
